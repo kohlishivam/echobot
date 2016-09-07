24ff7339a9ba940aca3ccab3f86644a9f330cccb
Shivam Kohli <shivamkohli@Shivams-MacBook-Pro.local>
2016-09-07 19:07:53 +0530

    hyyy

--- chatbot/views.py
+++ chatbot/views.py
@@ -14,7 +14,7 @@ import requests
 # Create your views here.
 
 VERIFY_TOKEN = '7thsep2016'
-PAGE_ACCESS_TOKEN = 'EAAWSz5ubdJ4BAJMGZAUrV01OaDdZA2GkYZBhKToKvvNFK7GDvcI6efhZCsqaP2ZCiPRbjFFZBbbXrcYnuqbvFCM8tQZCIk4jvvCBwixXlDXCjPoJOw8WxgbgthuUzQvI8VscZC6WO8DE2sUZAGIW6XuQZCq6ZAlJdEumOmktNP0i6MrEwZDZD'
+PAGE_ACCESS_TOKEN = '1972769552949846'
 
 def post_facebook_message(fbid,message_text):
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
